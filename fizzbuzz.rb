ee31dc829f380b8e2917b4a93710f7f744279d09
Monica Marziano <mmarziano1@gmail.com>
2018-01-27 12:34:46 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,19 @@
 def fizzbuzz(int)
-  if int % 3 == 0 
-    puts "Fizz"
-  elsif int % 5 == 0 
-    puts "Buzz"
-
-  elsif int % 15 == 0 
-    puts "FizzBuzz"
-  else int % 4 == 0
-    puts "No"
+  def fizz_3(input)
+    if input % 3 == 0 
+      puts "Fizz"
+    end
   end
-  
+  def fizz_5(input)
+    if input % 5 == 0 
+      puts "Buzz"
+    end
+  end
+  def fizz_15(input)
+    if input % 15 == 0 
+      puts "FizzBuzz"
+    end
+  end
 end
 
 fizzbuzz(5)
